70487ff76cc6cd0ca533290ec26ec4fc6fda4da8
ChangxuRen-github <79490821+ChangxuRen-github@users.noreply.github.com>
2023-03-17 18:16:05 -0700

    Update_the_latest_version

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,1 +1,104 @@
+import "./share.css";
+import {
+  PermMedia,
+  Label,
+  Room,
+  EmojiEmotions,
+  Cancel,
+} from "@material-ui/icons";
+import { useContext, useRef, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { ConstellationArray } from "../../context/Constellation";
 
+export default function Share() {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const desc = useRef();
+  const constellationSelect = useRef();
+  const [file, setFile] = useState(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+      constellation: constellationSelect.current.value,
+    };
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      newPost.img = fileName;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      await axios.post("/posts", newPost);
+      window.location.reload();
+    } catch (err) {}
+  };
+
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img
+            className="shareProfileImg"
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+          />
+          <input
+            placeholder={"What is in your mind " + user.username + "?"}
+            className="shareInput"
+            ref={desc}
+          />
+        </div>
+        <hr className="shareHr" />
+        {file && (
+          <div className="shareImgContainer">
+            <img className="shareImg" src={URL.createObjectURL(file)} alt="" />
+            <Cancel className="shareCancelImg" onClick={() => setFile(null)} />
+          </div>
+        )}
+        <form className="shareBottom" onSubmit={submitHandler}>
+          <div className="shareOptions">
+            <label htmlFor="file" className="shareOption">
+              <PermMedia htmlColor="tomato" className="shareIcon" />
+              <span className="shareOptionText">Photo or Video</span>
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="file"
+                accept=".png,.jpeg,.jpg"
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
+            <div className="shareOption">
+              <Label htmlColor="blue" className="shareIcon" />
+              <span className="shareOptionText">Select Constellation</span>
+              <select
+                name="constellation"
+                id="constellation"
+                ref={constellationSelect}
+              >
+                {ConstellationArray.map((e) => (
+                  <option value={e}>{e}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <button className="shareButton" type="submit">
+            Share
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
