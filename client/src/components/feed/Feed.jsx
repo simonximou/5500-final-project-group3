d12cb38e75d0429c1b13c268324707ae37e2ac6d
ChangxuRen-github <79490821+ChangxuRen-github@users.noreply.github.com>
2023-03-17 18:05:48 -0700

    Update_feed_new_version

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,1 +1,40 @@
+import { useContext, useEffect, useState } from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
 
+export default function Feed({ username, constellation = "", post = true }) {
+  const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      if (constellation != "") {
+        const res = await axios.get("/posts/" + constellation);
+        setPosts(res.data);
+      } else {
+        const res = username
+          ? await axios.get("/posts/profile/" + username)
+          : await axios.get("/posts/timeline/" + user._id);
+        setPosts(
+          res.data.sort((post1, post2) => {
+            return new Date(post2.createdAt) - new Date(post1.createdAt);
+          })
+        );
+      }
+    };
+    fetchPosts();
+  }, [username, user._id]);
+
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        {(!username || username === user.username) && post == true && <Share />}
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
+        ))}
+      </div>
+    </div>
+  );
+}
