6ee1e56345253ab2ef721dbf851d16af3c224745
Xi Zhang <71995672+simonximou@users.noreply.github.com>
2023-03-17 20:09:18 -0700

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,30 +2,6 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-//REGISTER
-router.post("/register", async (req, res) => {
-  try {
-    //generate new password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-    //create new user
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-      isFree: req.body.isFree,
-      isAdmin: req.body.isAdmin,
-      isVip:req.body.isVip,
-    });
-
-    //save user and respond
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
 
 //LOGIN
 router.post("/login", async (req, res) => {
