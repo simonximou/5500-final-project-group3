f5d8fd9096abb0f0dc0099701dfc85135f511107
ChangxuRen-github <79490821+ChangxuRen-github@users.noreply.github.com>
2023-04-27 22:11:47 -0700

    Update UI

--- client/src/components/constellation/index.jsx
+++ client/src/components/constellation/index.jsx
@@ -2,6 +2,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { constellationCall } from "../../apiCalls";
 import { useState, useEffect } from "react";
+import pic from "../constellation/1181682636155_.pic.jpg"
 const PF = process.env.REACT_APP_PUBLIC_FOLDER; // env variable that specifies the public folder that contains static assest
 
 export default function ({ constellation }) {
@@ -21,12 +22,19 @@ export default function ({ constellation }) {
   return (
     <div
       className="container"
-      style={{ paddingLeft: "30px", paddingRight: "30px", paddingTop: "10px" }}
+      style={{paddingLeft: "15px", paddingRight: "30px", paddingTop: "10px" }}
     >
+
       <div class="row">
-        <h1>Daily horoscopes for {constellation}</h1>
+        <div class="col-6">
+          <h1>Daily horoscopes art for {constellation}</h1>
+        </div>
+        <div className="col-6">
+          <h1>Join our community today!</h1>
+        </div>
       </div>
-      <div class="row">
+      <div class="row"
+           style={{paddingBottom: "50px" }}>
         <div class="col-sm">
           <img
             src={`${PF}constellation/${constellation}.jpeg`}
@@ -34,13 +42,21 @@ export default function ({ constellation }) {
           />
         </div>
 
-        <div class="col-sm" style={{ padding: "30px" }}>
-          <div class="row" id="today">
-            {data.today}
-          </div>
+        <div class="col-sm">
+          <img
+              src={pic}
+              style={{width: 300, height:300, borderRadius: 400/ 2}}
+          />
           <div class="row">{data.yesterday}</div>
         </div>
       </div>
+      <div className="row">
+        <img
+            src={'https://thumbs.dreamstime.com/b/magical-banner-astrology-celestial-alchemy-heavenly-art-zodiac-tarot-device-universe-crescent-moon-196472869.jpg'}
+        />
+      </div>
     </div>
   );
+}
+
 }
